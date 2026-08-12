536cf810fcbaafa1a76e84ef29d8052f6a0c9eca
Martinica Andrei-Marian <ni3r4ut0m4t4@gmail.com>
2026-08-12 12:20:10 +0300

    add menu in progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 const $ = document.querySelector.bind(document);
+const menu = $("#menu");
+const header = $("#header");
+
+const toggleMenu = (e) => {
+    const overflowHidden = "overflow-hidden";
+    if (menu.open) {
+        menu.close();
+        document.documentElement.classList.remove(overflowHidden);
+    }
+    else {
+        menu.show();
+        document.documentElement.classList.add(overflowHidden);
+    }
+}
